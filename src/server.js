ecdd03ba25082f28866b628fce8e5edaa5560a3c
hana <jjina.js@gmail.com>
2024-04-10 21:58:03 +0900

    웹소켓 세팅 완료

--- src/server.js
+++ src/server.js
@@ -1,21 +1,41 @@
 import express from "express";
+import path from "path";
+import http from "http";
+import WebSocket from "ws";
 
 /*
 Express : HTTP 요청을 처리하고 라우팅을 관리하며,
 클라이언트에게 응답을 제공하는 데 사용
 */
-
 const app = express();
 
+// views 디렉토리 설정
+app.set("views", path.join(__dirname, "views"));
+
+// 정적 파일 제공 (js, html, css 등)
+app.use(express.static(path.join(__dirname, "public")));
+
+// 뷰 엔진 설정 pug 파일이 뷰 엔진임
 app.set("view engine", "pug");
 
-// pug 파일의 경로 설정, pug 파일명은 "home"
-app.set("views", __dirname + "/views");
-app.get("/", (req, res) => res.render("home"));
-
-//  클라이언트로부터의 요청에 대한 응답을 제공하기 
-// 위해 라우팅을 설정하는 코드
-app.get("/", (req,res) => res.render("home"));
+// 홈 화면 렌더링
+app.get('/', (req, res) => res.render("home"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+
+// app.listen(3000, handleListen);
+
+// http 서버 위에 ws 서버 올림.
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+// connection이 이루어질 때마다, 소켓 통신이 이루어지게 됌. 
+function handleConnection(socket) {
+    console.log(socket);
+    console.log("소켓 연결");
+}
+wss.on("connection", handleConnection)
+
+
+
+server.listen(3000, handleListen);
