dfd7e72aefef096e63262a3a10cb9ff8c07fbe04
hana <jjina.js@gmail.com>
2024-04-12 12:51:18 +0900

    웹소켓을 활용한, 실시간 채팅 기능 구현 ( 여러 클라이언트 접속 가능 )

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -3,18 +3,32 @@
 const messageList = document.querySelector("ul");
 const messageForm = document.getElementById("myForm");
 const socket = new WebSocket(`ws://${window.location.host}`);
+const nickForm = document.getElementById("nick");
+
+// 백엔드와의 통신을 위해, JavaScript 객체를 JSON 문자열로 변환하는 함수이다.
+function makeMessage(type, payload){
+    const msg =  {type, payload};
+    return JSON.stringify(msg);
+}
 
 socket.addEventListener("open", () => {
     console.log("Connected To Server");
 })
 
-socket.addEventListener("message", (message) => {
-    console.log("New message : ", message.data , " from the server");
-});
+// socket.addEventListener("message", (message) => {
+//     console.log("New message : ", message.data , " from the server");
+// });
 
 socket.addEventListener("close", () => {
     console.log("DisConnected from Server");
-})
+});
+
+// 메시지를 받으면, 화면에 li를 하나씩 추가해줌
+socket.addEventListener("message", (message) => {
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li);
+});
 
 setTimeout(() => {
     socket.send("hello from the browser!");
@@ -23,8 +37,20 @@ setTimeout(() => {
 function handleSubmit(event){
     event.preventDefault();
     const input = messageForm.querySelector("#messageInput");
-    console.log("Input:" , input.value);
-    socket.send(input.value);
+    // console.log("Input:" , input.value);
+
+    // 서버 단에서 받은 메세지를 다시 프론트 단에서 서버 단으로 넘기면, 양방향 통신
+    socket.send(makeMessage("new_message", input.value));
 }
 
 messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", (event) => {
+
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+    const m = makeMessage("nickname", input.value);
+    // string 형 json으로 넘겨지는 것 확인 가능
+    console.log(m);
+    socket.send( m );
+
+})
--- src/server.js
+++ src/server.js
@@ -23,32 +23,56 @@ app.get('/', (req, res) => res.render("home"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
-// app.listen(3000, handleListen);
-
 // http 서버 위에 ws 서버 올림.
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-// // connection이 이루어질 때마다, 소켓 통신이 이루어지게 됌. 
-// function handleConnection(socket) {
-//     // socket 이 frontend와 real-time 소통을 가능하게 함. 
-//     console.log(socket);
-//     console.log("소켓 연결");
-//     // 서버의 소켓은 연결된 브라우저를 뜻함 
-// }
-// 소켓이 제공하는 여러 함수들이 있음 
+
+// 아래의 코드는 웹 소켓 서버에서 클라이언트 간에 메시지를 전달하는 기능을 구현한 것입니다. 각 클라이언트의 웹 소켓 연결이 생성되면 서버는 해당 클라이언트의 웹 소켓을 clients 객체에 추가합니다. 그 후, 클라이언트가 메시지를 보내면 서버는 해당 메시지를 받아들이고, 이를 다시 연결된 모든 클라이언트에게 전달합니다.
+
+// 기능 요약:
+
+// 새로운 클라이언트가 서버에 연결되면, 해당 클라이언트의 웹 소켓을 clients 객체에 추가합니다.
+// 클라이언트가 메시지를 보내면, 서버는 해당 메시지를 받아들이고, 모든 연결된 클라이언트에게 해당 메시지를 전달합니다.
+// 클라이언트의 연결이 종료되면, 해당 클라이언트의 웹 소켓을 clients 객체에서 제거합니다.
+
+// 웹 소켓 서버에서 새로운 클라이언트가 연결될 때마다 한 객체씩 배열에 넣어줌
+const sockets = [];
 
 wss.on("connection", (socket) => {
-    console.log("Connected to Browser");
+    
+    sockets.push(socket);
+    socket["nickname"] = "Anon";
     socket.on("close", () => console.log("Disconnected from the Browser"));
+    
+    // 웹소켓은 데이터를 전송할 때 기본적으로 이진 데이터를 사용함.
+    // 따라서, json 형식으로 문자열을 보내도 서버에서는 이를 이진 데이터로 수신
+    // 따라서, 서버에서 수신된 메시지를 처리 하기 전에, 먼저 이진 데이터를 문자열로 변환 한 후,
+    // JSON.parse() 메서드를 사용하여 JavaScript 객체로 변환해야함. 
     socket.on("message", (message) => {
+        // 이진 데이터를 문자열로 처리
         const messageString = message.toString('utf-8');
-        socket.send(messageString);
         console.log(messageString);
+
+        // 문자열을 자바스크립트 객체로 변환
+        const parsed = JSON.parse(messageString);
+        console.log(parsed);
+
+        // 프론트에 전송
+        switch(parsed.type){
+            case "new_message" :
+                //sockets.forEach((aSocket) => aSocket.send(parsed.payload)); 이건 그냥 들어오는 값의 payload를 반환
+                // 아래는 소켓에서 초기화해놓은, 닉네임인 Anon에, 자신이 작성한 메시지가 추가가 됌.
+                sockets.forEach((aSocket) => aSocket.send(`${socket.nickname} : ${parsed.payload}`));
+                break;
+            case "nickname" :
+                // 아래는 Anon이 아닌, 자신이 입력한 닉네임(payload)의 값이 입력되게 됌. 
+                socket["nickname"] = parsed.payload;
+                break;
+        }
+
     });
-    socket.send("hello!!");
+
 });
-
-
 
 server.listen(3000, handleListen);
